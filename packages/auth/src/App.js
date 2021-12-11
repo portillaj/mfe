aef595ae755e9f411b79153fe3b01de0d9a0581c
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2021-12-11 08:47:00 -0500

    made style change

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -11,21 +11,19 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "au",
 });
 
-export default ({ history, onSignIn }) => {
-  return (
-    <div>
-      <StylesProvider generateClassName={generateClassName}>
-        <Router history={history}>
-          <Switch>
-            <Route path="/auth/signin">
-              <Signin onSignIn={onSignIn} />
-            </Route>
-            <Route path="/auth/signup">
-              <Signup onSignIn={onSignIn} />
-            </Route>
-          </Switch>
-        </Router>
-      </StylesProvider>
-    </div>
-  );
-};
+export default ({ history, onSignIn }) => (
+  <div>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route path="/auth/signin">
+            <Signin onSignIn={onSignIn} />
+          </Route>
+          <Route path="/auth/signup">
+            <Signup onSignIn={onSignIn} />
+          </Route>
+        </Switch>
+      </Router>
+    </StylesProvider>
+  </div>
+);
